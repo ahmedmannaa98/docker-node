a9bd2fe8cb20bf551482962303c2e3da4cbabf30
ahmedmannaa98 <ayousef@easyapplyedu.com>
2024-10-27 13:36:53 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -8,12 +8,6 @@ const app = express()
 
 const envVars = process.env
 
-// console.log(`postgresql://${envVars.POSTGRES_USER}:${envVars.POSTGRES_PASSWORD}@${envVars.POSTGRES_HOST}:5432`)
-// const connectionString = `postgresql://${envVars.POSTGRES_USER}:${envVars.POSTGRES_PASSWORD}@${envVars.POSTGRES_HOST}:5423/test-db`
-// const client = new Client({connectionString})
-// client.connect().then(()=> console.log("postgres connected ...")).catch((err)=> console.log(err))
-
-
 mongoose.connect(`mongodb://${envVars.MONGO_USERNAME}:${envVars.MONGO_PASSWORD}@${envVars.MONGO_HOST}:27017/dev-db?authSource=admin`)
 .then(()=> console.log("mongo connected ..."))
 .catch((err)=> console.log(err))
